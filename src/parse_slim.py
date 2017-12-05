6bec15c526281f2b10a61f4bba5bd535eb465fed
Daniel Rice <daniel.paul.rice@gmail.com>
2017-12-05 10:07:19 -0600

    Add slim output parsing to pipeline

--- src/parse_slim.py
+++ src/parse_slim.py
@@ -19,12 +19,12 @@ else:
 fn_split = filename.split('.')
 outfn = '.'.join(fn_split[:-1]) + '.wsfs.' + fn_split[-1] + '.gz'
 
-sys.stderr.write('Importing data...\n')
+# sys.stderr.write('Importing data...\n')
 sample_size, positions, allele_counts = h.import_slim_output(filename)
 minor_allele_counts = np.minimum(allele_counts, sample_size - allele_counts)
 
-sys.stderr.write('Computing windowed statistics...\n')
+# sys.stderr.write('Computing windowed statistics...\n')
 sfs_w = h.windowed_sfs(positions, minor_allele_counts, sample_size, L, window_size)
 
-sys.stderr.write('Saving to file...\n')
+# sys.stderr.write('Saving to file...\n')
 np.savetxt(outfn, sfs_w, fmt='%d', header='window_size={}'.format(window_size))
