e44d5c5b96fe738cff1b4f4b4850986af2435e99
Daniel Rice <daniel.paul.rice@gmail.com>
2017-12-08 16:48:28 -0600

    Minor clean-up of the msprime simulation script

--- src/simulate_joint_sfs.py
+++ src/simulate_joint_sfs.py
@@ -6,7 +6,6 @@ import argparse
 # sys.path.insert(1, '/users/danielrice/msprime-lambda/')
 sys.path.insert(1, '/home/dpr/mmc_genomics/src/msprime-lambda/')
 import msprime
-# import sfs
 import jsfs
 
 # Constant for the length of time for a bottleneck to last
@@ -88,9 +87,6 @@ if args.error_rate and not args.mutation_rate:
     sys.stderr.write("ERROR: must specify a mutation rate to have errors!\n")
     sys.exit(1)
 
-# TODO: Allow this to vary in the future?
-n_sites = 1
-
 # TODO: Make sure all the parameter outputs conform to the new standards
 sys.stdout.write('#N_SAMPLES={}\n'.format(args.nSamples))
 sys.stdout.write('#N_LOCI={}\n'.format(','.join([str(nL) for nL in args.nLoci])))
@@ -115,6 +111,7 @@ demographic_events = []
 
 # Population structure
 if args.demes:
+    # TODO: check migration rate normalizations
     if args.migration_type == 'island':
         # Normalize mutation rate to a per-generation per-deme basis
         m = args.migration_rate / (4.0 * (args.demes - 1.0))
@@ -129,12 +126,14 @@ if args.demes:
     migration_matrix *= m
     migration_matrix = migration_matrix.tolist()
     samples_per_deme = args.nSamples / args.demes
-    population_configurations = [msprime.PopulationConfiguration(sample_size=samples_per_deme,
-                                         growth_rate=args.growth_rate,
-                                         multiple_merger_para=args.alpha) 
+    population_configurations = [msprime.PopulationConfiguration(
+                                        sample_size=samples_per_deme,
+                                        growth_rate=args.growth_rate,
+                                        multiple_merger_para=args.alpha) 
                                     for d in range(args.demes)]
     sys.stdout.write('#DEMES={}\n#MIGRATION_RATE={}\n'.format(args.demes, args.migration_rate))
     sys.stdout.write('#MIGRATION_TYPE={}\n'.format(args.migration_type))
+
 else:
     population_configurations = [msprime.PopulationConfiguration(sample_size=args.nSamples,
                                     growth_rate=args.growth_rate,
@@ -150,7 +149,7 @@ else:
         # FIXME: don't hard-code the post-bottleneck size
         for tb, strength in zip(args.bottleneck_time, args.bottleneck_strength):
             b_size = BOTTLENECK_DURATION / strength
-            demographic_events.append(msprime.PopulationParametersChange(tb, initial_size=b_size/2.0))
+            demographic_events.append(msprime.PopulationParametersChange(tb, initial_size=b_size/2))
             demographic_events.append(msprime.PopulationParametersChange(tb+BOTTLENECK_DURATION, initial_size=0.5))
         sys.stdout.write('#BOTTLENECK_TIMES={}\n'.format(','.join([str(tb) for tb in args.bottleneck_time])))
         sys.stdout.write('#BOTTLENECK_STRENGTHS={}\n'.format(','.join([str(s) for s in args.bottleneck_strength])))
@@ -162,17 +161,8 @@ if args.individual_loci:
     sys.stdout.write(header)
 
 # RUN SIMULATION
-# TAU = np.zeros((sum(args.nLoci), args.nSamples - 1))
-# TAU = np.zeros((sum(args.nLoci), args.nSamples - 1))
-# JSFS = np.zeros((sum(args.nLoci), args.nSamples, args.nSamples))
-# mTAU  = np.zeros((sum(args.nLoci), args.nSamples - 1))
-# jTAU = np.zeros((sum(args.nLoci), args.nSamples - 1, args.nSamples - 1))
-
-# TAU1  = np.zeros((sum(args.nLoci), args.nSamples - 1))
-# TAU2  = np.zeros((sum(args.nLoci), args.nSamples - 1))
 mTAU  = np.zeros((args.nSamples - 1))
 jTAU  = np.zeros((args.nSamples -1, args.nSamples - 1))
-# TAU2  = np.zeros((sum(args.nLoci), args.nSamples - 1))
 cumulative_loci = 0
 for nLoci, Tc in zip(args.nLoci, args.coalescent_time):
     sys.stderr.write('{} loci with Tc={}\n'.format(nLoci, Tc))
@@ -192,9 +182,6 @@ for nLoci, Tc in zip(args.nLoci, args.coalescent_time):
             tau2, _ = jsfs.get_jsfs(rep2)
             mTAU += tau1
             mTAU += tau2
-            # FIXME: debug
-            # if tau1[40] > 0 and tau2[40] > 0:
-            #     sys.stderr.write("{}\t{}\t{}\n".format(tau1[40], tau2[40], tau1[40]*tau2[40]))
             jTAU += tau1[None,:] * tau2[:,None]
 
     else:
@@ -229,84 +216,6 @@ sys.stderr.write('Calculating average...\n')
 mTAU /= 2.0*cumulative_loci
 jTAU += jTAU.T
 jTAU /= 2.0*cumulative_loci
-# mTAU = np.mean((TAU1+TAU2)/2.0, axis=0)
-# jTAU = np.mean(TAU1[:,None,:]*TAU2[:,:,None], axis=0)
-# jTAU = (jTAU + jTAU.T)/2.0
 
 sys.stdout.write(' '.join([str(x) for x in mTAU]) + '\n')
 sys.stdout.write(' '.join([str(x) for x in jTAU[np.triu_indices(args.nSamples-1)]]) + '\n')
-
-# # FIXME:
-# # np.set_printoptions(precision=2)
-# # print np.log(jTAU/(mTAU[:,None]*mTAU[None,:]))
-# import matplotlib.pyplot as plt
-# n = args.nSamples
-
-# # x = np.arange(1.0/n,1,1.0/n)
-# # plt.plot(x, (np.diagonal(jTAU)/mTAU**2)/n, '.-')
-# # plt.plot(x, x, '--k')
-# # plt.show()
-
-# # plt.plot(x[:9], jTAU[10,11:20]/(mTAU[11:20]*mTAU[10]), '.-')
-# # plt.show()
-
-# mTAU_folded = (mTAU + mTAU[::-1])[:n/2]
-# if n % 2 == 0:
-#     mTAU_folded[-1] /= 2.0
-# print mTAU_folded
-
-# jTAU_folded = (jTAU + jTAU[::-1,:] + jTAU[:,::-1])[:n/2,:n/2]
-# if n % 2 == 0:
-#     jTAU_folded[-1,:-1] /= 2.0
-#     jTAU_folded[:-1,-1] /= 2.0
-#     jTAU_folded[-1,-1] /= 3.0
-
-# mTAU_sq = mTAU_folded[None,:]*mTAU_folded[:,None]
-# ratio = jTAU_folded / mTAU_sq
-# print jTAU_folded
-# x = np.arange(1.0, n/2+1, 1.0)
-# plt.plot(x, np.diagonal(jTAU_folded)/mTAU_folded**2, '.-')
-# plt.plot(x, x/2.0, '--k')
-# plt.show()
-
-# for o in range(1,5):
-# for o in range(1,n/2-2):
-#     plt.semilogx(o, np.nanmean(np.diagonal(ratio,offset=o)), '.k')
-#     # c = str(1.0 - o/6.0)
-#     # plt.plot(x[:-o], np.diagonal(ratio,offset=o), '.-', color=c)
-# plt.show()
-
-# jTAU_flipped = jTAU_folded[::-1,:]
-# mTAU_sq = mTAU_folded[:,None]*mTAU_folded[None,:]
-# mTAU_flipped = mTAU_sq[::-1,:]
-
-# for o in range(0,11,2):
-#     c = str(1.0 - o/12.0)
-#     plt.plot(x[o:] - o/2, np.diagonal(jTAU_flipped,-o)/np.diagonal(mTAU_flipped,-o), '.-', color=c)
-# plt.show()
-
-# for o in range(0,11,2):
-#     c = str(1.0 - o/12.0)
-#     plt.plot(x[o:] - o/2, np.diagonal(jTAU_flipped,o)/np.diagonal(mTAU_flipped,o), '.-', color=c)
-# plt.show()
-
-# plt.pcolor(np.log(jTAU_folded/(mTAU_folded[:,None]*mTAU_folded[None,:])))
-# plt.colorbar()
-# plt.show()
-
-
-# JSFS += np.transpose(JSFS, axes=(0,2,1))
-# JSFS /= np.sum(JSFS, axis=(1,2))[:,None,None]
-# avgJSFS = np.mean(JSFS, axis=0)
-# avgMSFS = np.sum(avgJSFS, axis=1)
-# print avgMSFS
-# print avgJSFS
-# MI = np.sum(avgJSFS * np.log2(avgJSFS / (avgMSFS[None,:]*avgMSFS[:,None])))
-# print MI / (args.mutation_rate*Tc)**2
-# sys.stdout.write(' '.join([str(x) for x in avgJSFS[np.triu_indices(args.nSamples)]]) + '\n')
-
-    # M1 = np.mean(TAU, axis=0)
-    # M2 = np.mean(TAU[:,None,:]*TAU[:,:,None], axis=0)
-    # print ' '.join([str(x) for x in M1])
-    # # Print second moment matrix in condensed form (only the upper triangular portion)
-    # print ' '.join([str(x) for x in M2[np.triu_indices(M2.shape[0])]])
